a352e24a820a2d3dc2408c9825c89099ff6d1886
Viktoriia-Kovalska <kovalskayavi853@gmail.com>
2024-12-17 15:27:48 +0200

    corrected link

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -7,7 +7,7 @@ const PIXABAY_KEY = '47506069-50a24acca197abc732faaa7f2';
 const URL_PIXABAY = 'https://pixabay.com/api/';
 
 export async function fetchPixabay(term, page = 1) {
-  const response = await axios(`https://pixabay.com/api`, {
+  const response = await axios(`https://pixabay.com/api/`, {
     params: {
       key: PIXABAY_KEY,
       q: term,
